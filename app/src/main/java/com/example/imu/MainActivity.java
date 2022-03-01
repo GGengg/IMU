82befd1af1efc44a53eb6aa71c2a7a7870dd5b47
Geng <wanggeng0817@gmail.com>
2022-03-01 18:58:15 +1100

    Calculated orientation angle and added editText for user-input server url

--- app/src/main/java/com/example/imu/MainActivity.java
+++ app/src/main/java/com/example/imu/MainActivity.java
@@ -8,9 +8,11 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Handler;
 import android.os.SystemClock;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.util.Log;
 import java.io.IOException;
@@ -36,6 +38,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             magxText, magyText, magzText, Timestamp;
 
     public float accx,accy,accz,gyrox,gyroy,gyroz,magx,magy,magz;
+
+    private final float[] LastAccReading = new float[3];
+    private final float[] LastMagReading = new float[3];
+    private final float[] rotationMatrix = new float[9];
+    private final float[] orientationAngles = new float[3];
+
     public long time;
 
     @Override
@@ -59,6 +67,43 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         sensors.put("Gyroscope",sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE));
         sensors.put("Magnetic",sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD));
         registerSensors();
+        Update_UI();
+    }
+
+    public void Update_UI(){
+        Handler Update_UI_Handler = new Handler();
+        Runnable Update_UI_Runnable = new Runnable() {
+            @Override
+            public void run() {
+                if (!Running) {
+                    Update_UI_Handler.removeCallbacks(this);
+                } else {
+                    Update_UI_Handler.postDelayed(this,200);
+
+                    String AccX = getString(R.string.AccelerometerX,accx);
+                    String AccY = getString(R.string.AccelerometerY,accy);
+                    String AccZ = getString(R.string.AccelerometerZ,accz);
+                    accxText.setText(AccX);
+                    accyText.setText(AccY);
+                    acczText.setText(AccZ);
+
+                    String MagX = getString(R.string.Magnetic_FieldX,magx);
+                    String MagY = getString(R.string.Magnetic_FieldY,magy);
+                    String MagZ = getString(R.string.Magnetic_FieldZ,magz);
+                    magxText.setText(MagX);
+                    magyText.setText(MagY);
+                    magzText.setText(MagZ);
+
+                    String GyroX = getString(R.string.GyroscopeX,gyrox);
+                    String GyroY = getString(R.string.GyroscopeY,gyroy);
+                    String GyroZ = getString(R.string.GyroscopeZ,gyroz);
+                    gyroxText.setText(GyroX);
+                    gyroyText.setText(GyroY);
+                    gyrozText.setText(GyroZ);
+                }
+            }
+        };
+        Update_UI_Handler.postDelayed(Update_UI_Runnable,100);
     }
 
     public void registerSensors(){
@@ -78,99 +123,99 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         Log.d(TAG,"onClickLogData");
         final OkHttpClient client = new OkHttpClient();
 
-        Handler handler = new Handler();
-        Runnable runnable = new Runnable() {
-            @Override
-            public void run() {
-                if (Running) {
-                    handler.postDelayed(this, 20);
-                    String url = "http://192.168.86.43:5000/server";
-                    Log.d(TAG, String.valueOf(time));
-                    RequestBody body = new FormBody.Builder()
-                            .add("Timestamp", String.valueOf(time))
-                            .add("accx", String.valueOf(accx))
-                            .add("accy", String.valueOf(accy))
-                            .add("accz", String.valueOf(accz))
-                            .add("gyrox", String.valueOf(gyrox))
-                            .add("gyroy", String.valueOf(gyroy))
-                            .add("gyroz", String.valueOf(gyroz))
-                            .add("magx", String.valueOf(magx))
-                            .add("magy", String.valueOf(magy))
-                            .add("magz", String.valueOf(magz))
-                            .build();
+        EditText url_text = findViewById(R.id.editTextNumber);
+        String url_bit = url_text.getText().toString();
 
-                    Request request = new Request.Builder()
-                            .url(url)
-                            .post(body)
-                            .build();
+        String url = "http://192.168.86." + url_bit + ":5000/server";
 
-                    final Call call = client.newCall(request);
-                    call.enqueue(new Callback() {
-                        @Override
-                        public void onFailure(@NonNull Call call, @NonNull IOException e) {
-                            Log.i("onFailure", e.getMessage());
-                        }
+        //String url = "http://192.168.86.48:5000/server";
 
-                        @Override
-                        public void onResponse(@NonNull Call call, @NonNull Response response)
-                                throws IOException {
-                            assert response.body() != null;
-                            String result = response.body().string();
-                            Log.i("result", result);
-                        }
-                    });
-                } else {
-                    handler.removeCallbacks(this);
+        Thread thread = new Thread(() -> {
+            while (Running) {
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
-                }
-        };
-        handler.postDelayed(runnable, 1000);
+
+                RequestBody body = new FormBody.Builder()
+                        .add("Timestamp", String.valueOf(time))
+                        .add("accx", String.valueOf(accx))
+                        .add("accy", String.valueOf(accy))
+                        .add("accz", String.valueOf(accz))
+                        .add("gyrox", String.valueOf(gyrox))
+                        .add("gyroy", String.valueOf(gyroy))
+                        .add("gyroz", String.valueOf(gyroz))
+                        .add("magx", String.valueOf(magx))
+                        .add("magy", String.valueOf(magy))
+                        .add("magz", String.valueOf(magz))
+                        .add("orientation", orientationAngles[0] + " " +
+                                orientationAngles[1] + " " + orientationAngles[2])
+                        .build();
+
+                Log.d(TAG,orientationAngles[0] +
+                        String.valueOf(orientationAngles[1]) +
+                        orientationAngles[2]);
+
+                Request request = new Request.Builder()
+                        .url(url)
+                        .post(body)
+                        .build();
+
+                final Call call = client.newCall(request);
+                call.enqueue(new Callback() {
+                    @Override
+                    public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                        Log.i("onFailure", e.getMessage());
+                    }
+
+                    @Override
+                    public void onResponse(@NonNull Call call, @NonNull Response response)
+                            throws IOException {
+                        assert response.body() != null;
+                        String result = response.body().string();
+                        Log.i("result", result);
+                    }
+                });
+            }
+        });
+        thread.start();
     }
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
-        time = SystemClock.elapsedRealtime();
-        Timestamp.setText(String.valueOf(time));
-
         //in milliseconds since last boot
-        //Log.d(TAG, String.valueOf(SystemClock.elapsedRealtime()));
+        time = SystemClock.elapsedRealtime();
+        //Timestamp.setText(String.valueOf(time));
 
         switch (sensorEvent.sensor.getType()){
             case Sensor.TYPE_ACCELEROMETER:
+                System.arraycopy(sensorEvent.values,0,LastAccReading,0,sensorEvent.values.length);
                 accx = sensorEvent.values[0];
                 accy = sensorEvent.values[1];
                 accz = sensorEvent.values[2];
-                String AccX = this.getString(R.string.AccelerometerX,accx);
-                String AccY = this.getString(R.string.AccelerometerY,accy);
-                String AccZ = this.getString(R.string.AccelerometerZ,accz);
-                accxText.setText(AccX);
-                accyText.setText(AccY);
-                acczText.setText(AccZ);
                 break;
 
             case Sensor.TYPE_MAGNETIC_FIELD:
+                System.arraycopy(sensorEvent.values,0,LastMagReading,0,sensorEvent.values.length);
                 magx = sensorEvent.values[0];
                 magy = sensorEvent.values[1];
                 magz = sensorEvent.values[2];
-                String MagX = this.getString(R.string.Magnetic_FieldX,magx);
-                String MagY = this.getString(R.string.Magnetic_FieldY,magy);
-                String MagZ = this.getString(R.string.Magnetic_FieldZ,magz);
-                magxText.setText(MagX);
-                magyText.setText(MagY);
-                magzText.setText(MagZ);
                 break;
 
             case Sensor.TYPE_GYROSCOPE:
                 gyrox = sensorEvent.values[0];
                 gyroy = sensorEvent.values[1];
                 gyroz = sensorEvent.values[2];
-                String GyroX = this.getString(R.string.GyroscopeX,gyrox);
-                String GyroY = this.getString(R.string.GyroscopeY,gyroy);
-                String GyroZ = this.getString(R.string.GyroscopeZ,gyroz);
-                gyroxText.setText(GyroX);
-                gyroyText.setText(GyroY);
-                gyrozText.setText(GyroZ);
         }
+
+        // Rotation matrix based on current readings from accelerometer and magnetometer.
+        SensorManager.getRotationMatrix(rotationMatrix, null,
+                LastAccReading, LastMagReading);
+        // Express the updated rotation matrix as three orientation angles.
+        SensorManager.getOrientation(rotationMatrix, orientationAngles);
+
+        //Log.i("OrientationTestActivity",String.format("Orientation: %f, %f, %f", orientationAngles[0],orientationAngles[1],orientationAngles[2]));
     }
 
     @Override
